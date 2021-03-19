0f8d9959987dd83bb2c34c691bb1f4deb327bd72
Srikanth Kalyanam <kalyanamsrikanthcnc@gmail.com>
2021-03-19 17:55:20 +0530

    enhanced ps1, added py

--- pinger.ps1
+++ pinger.ps1
@@ -1,24 +1,28 @@
 ﻿$pswindow = (get-host).ui.rawui
 
-#$newsize = $pswindow.buffersize
-#$newsize.height = 3000
-#$newsize.width = 150
-#$pswindow.buffersize = $newsize
-$newsize = $pswindow.windowsize
-$newsize.height = 5
-$newsize.width = 25
-$pswindow.windowsize = $newsize
+if ($true){
+    $newsize = $pswindow.windowsize
+    $newsize.height = 4
+    $newsize.width = 30
+    $pswindow.windowsize = $newsize
+
+    $newsize = $pswindow.buffersize
+    $newsize.height = 100
+    $newsize.width = 30
+    $pswindow.buffersize = $newsize
+}
 
 if ( [bool](get-job -Name pinger -ea silentlycontinue) )
 {
     stop-job -name pinger
     receive-job -name pinger
 }
+$threshold = 1000
 $pinger = {
     $ms = 0
     while ($true){
         Test-Connection -ComputerName 8.8.8.8 -Quiet -OutVariable conn -Count 1 > $null
-        if ($conn -eq $true){
+        if ($conn -eq 1000){ #$threshold
             if ($ms -le 1000){ 
                 $ms += 100
             }
@@ -39,24 +43,36 @@ $job = Start-Job -Name 'pinger' -ScriptBlock $pinger
 #Debug-Job $job
 
 $count = 0
-$char = '.'
+$sleepms = 100
+$char = '?'
 while ($true){
     $conn = Get-Job -Name pinger | Receive-Job
     if ($conn -eq $true){
-        Write-Host '|'$count
+        #Write-Host '|'$count
+        Write-Host 'ok'
         $char = '.'
-        $count = 0
     }
     elseif ($conn -eq $false){
-        Write-Host '[x]'$count
+        #Write-Host '[x]'$count
+        Write-Host '[x]NA'
         $char = 'x'
-        $count = 0
     }
     elseif ($conn -ne $null){
         Write-Host $conn
+    }
+
+    if ($conn -ne $null){
+        Write-Host -NoNewline 'Internet'
         $count = 0
     }
     Write-Host -NoNewline $char
     $count += 1
-    Sleep -Milliseconds 100
+    Sleep -Milliseconds $sleepms
+    if ($char -ne 'x'){
+        if ($sleepms*$count -lt $threshold){
+            $char = '.'
+        }else{
+            $char = '?'
+        }
+    }
 }
